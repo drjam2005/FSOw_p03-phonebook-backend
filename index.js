fd0bfbdc9535ecd316b0aaa51431380d60c70260
drjam2005 <jamesallanmdr2005@gmail.com>
2026-07-12 19:34:41 +0800

    chore: finished exercises 3.19 -> 3.21

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static('dist'));
 morgan.token('body', (req) => { return JSON.stringify(req.body)});
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+morgan.token('time', () => { return new Date().toISOString(); });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :time :body'));
 
 app.get('/', (request, response) => {
 	response.send(` <h1> Hello, World!  </h1> `);
@@ -41,7 +42,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 		.catch(error => next(error));
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const { name, number } = request.body;
 
 	if(!request.body){
@@ -61,9 +62,9 @@ app.post('/api/persons', (request, response) => {
 		number: number
 	});
 
-	newPerson.save().then(() => {
-		return response.json(newPerson);
-	});
+	newPerson.save().then(savedPerson => {
+		return response.json(savedPerson);
+	}).catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -116,6 +117,8 @@ const errorHandler = (error, request, response, next) => {
 		return response.status(400).send({
 			error: 'malformed id'
 		});
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
 
 	next(error);
--- models/person.js
+++ models/person.js
@@ -12,9 +12,23 @@ mongoose.connect(url, { family: 4 })
 	});
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String
+	name: {
+		type: String,
+		minlength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		required: [true, 'User phone number required!'],
+		validate: {
+			validator: v => {
+				return /^\d{2,3}-\d{5,}$/.test(v);
+			},
+			message: props => `${props.value} is not a valid phone number!`
+		}
+	}
 });
+
 
 personSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
